8ad954f3cf3c9ea736a6594bacfd08b64cd71875
Ahmet2001 <145967670+Ahmet2001@users.noreply.github.com>
2023-12-17 21:26:04 +0300

    Time: 68 ms (83.28%) | Memory: 7 MB (58.30%) - LeetSync

--- 1-two-sum/two-sum.c
+++ 1-two-sum/two-sum.c
@@ -2,7 +2,7 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
      int *k =(int*) calloc(2,sizeof(int));
     * returnSize = 2;
      int n = numsSize;
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n ;  i++) {
           int x = target - nums[i];
             for (int j = i + 1; j < n; j++) {
               if (x == nums[j])
